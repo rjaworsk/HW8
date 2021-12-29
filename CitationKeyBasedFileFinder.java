199dd450c2a3a551308e362a514577617c3c8a93
rjaworsk <92691704+rjaworsk@users.noreply.github.com>
2021-12-29 12:48:11 +0100

    changed a comment

--- CitationKeyBasedFileFinder.java
+++ CitationKeyBasedFileFinder.java
@@ -41,7 +41,7 @@ package Task2;
 
 	        List<Path> result = new ArrayList<>();
  
-	        // First scan directories HII
+	        // First scan directories Hello
 	        Set<Path> filesWithExtension = findFilesByExtension(directories, extensions);
 
 	        // Now look for keys
